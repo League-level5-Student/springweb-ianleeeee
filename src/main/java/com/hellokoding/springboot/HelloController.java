e09eee67be49f357c47b33e616d276d4b8732e43
DavidAndBlake <League@AdministorsiMac.attlocal.net>
2017-11-26 14:31:43 -0800

    Spring boot thing

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -1,5 +1,8 @@
 package com.hellokoding.springboot;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping("/wow")
     public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
+        model.addAttribute("name", scramb(name.toLowerCase()));
         return "hello";
     }
+    public static String scramb(String name) {
+		char []c = name.toCharArray();
+		ArrayList<Character> l = new ArrayList();
+		for (char character : c) {
+			l.add(character);
+		}
+		Collections.shuffle(l);
+		String finalS = "";
+		for (Character x : l) {
+			finalS+=x;
+		}
+		return finalS;
+	}
+   
 }
